67add8d48896dfdffeaef09f1e0065db50d5a6a2
dev-abhisheksh <abhisheksharmaazpm2@gmail.com>
2026-03-17 00:12:04 +0530

    feat: update index.html title and comment out favicon link; enhance Button component to support onClick prop

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const Button = ({ text, className }) => {
+const Button = ({ text, className, onClick }) => {
     return (
-        <button className={`px-3 py-2 rounded-md font-medium ${className}`}>
+        <button onClick={onClick} className={`px-3 py-2 rounded-md font-medium ${className}`}>
             {text}
         </button>
     );
