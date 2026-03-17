8161b29ec3ac97ac1b1aee1e76f8f014035763e2
dev-abhisheksh <abhisheksharmaazpm2@gmail.com>
2026-03-17 21:28:06 +0530

    refactor: remove unused files and clean up chrome extension structure

--- chrome-extension/background.js
+++
@@ -1,5 +0,0 @@
-chrome.runtime.onMessage.addListener((msg) => {
-    if (msg.token) {
-        chrome.storage.local.set({ token: msg.token });
-    }
-});
--- chrome-extension/content.js
+++
@@ -1,8 +0,0 @@
-// content.js
-const token = localStorage.getItem("token");
-
-console.log("Content script running, token:", token);
-
-if (token) {
-    chrome.runtime.sendMessage({ token });
-}
