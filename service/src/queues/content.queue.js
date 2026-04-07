9b8b614547ae12f384a45eb2daf32dc32a048ef7
dev-abhisheksh <abhisheksharmaazpm2@gmail.com>
2026-04-07 15:28:02 +0530

    feat: add default job options for content processing queue

--- service/src/queues/content.queue.js
+++ service/src/queues/content.queue.js
@@ -2,5 +2,8 @@ import { Queue } from "bullmq";
 import redisClient from "../config/redisClient.js";
 
 export const contentQueue = new Queue("content-processing", {
-    connection: redisClient
+    connection: redisClient,
+    defaultJobOptions: {
+        removeOnComplete: true,
+    }
 })
